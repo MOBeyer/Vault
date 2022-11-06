80fb13424b8d4db4ca8e7464a9ca5c33ccc0e90b
Moritz Beyer <moritz.orlando.beyer@mac.com>
2022-11-06 18:58:31 +0100

    Preview

--- Apple/SwiftUI/Swift_Playground.playground/Contents.swift
+++ Apple/SwiftUI/Swift_Playground.playground/Contents.swift
@@ -1,3 +1,18 @@
 import SwiftUI
+import PlaygroundSupport
 
 var greeting = "Hello, playground"
+
+struct ContentView: View {
+    var body: some View {
+        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+PlaygroundPage.current.setLiveView(ContentView())
---
+++ HWS/SwiftUI/project1/project1/project1App.swift
@@ -0,0 +1,17 @@
+//
+//  project1App.swift
+//  project1
+//
+//  Created by Main Hub on 06.11.22.
+//
+
+import SwiftUI
+
+@main
+struct project1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
