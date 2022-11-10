0924f092b50d47c8d207077d83c35e7a5959019e
MOBeyer <moritz.orlando.beyer@mac.com>
2022-11-10 12:59:58 +0100

    XCTest Project1

---
+++ Apple/Frameworks/XCTest/project1/project1/project1App.swift
@@ -0,0 +1,17 @@
+//
+//  project1App.swift
+//  project1
+//
+//  Created by Moritz Beyer on 10.11.22.
+//
+
+import SwiftUI
+
+@main
+struct project1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
